c33b92a68310c3fdbb4705476f2abda627302126
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-11-17 18:40:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+
+  const oneDay = 24 * 60 * 60 * 1000; // hours * minutes * seconds * milliseconds
+
+  // Convert both dates to milliseconds
+  const firstDate = new Date(date1);
+  const secondDate = new Date(date2);
+
+  // Calculate the difference in milliseconds
+  const diffInMilliseconds =  secondDate - firstDate;
+
+    return Math.round(diffInMilliseconds / oneDay);
+
+  
+	
 };
 
-// Do not change the code below.
+
+
+// Do not change the code below   .    
 const dateOne = prompt("Enter Start Date.");
 const dateTwo = prompt("Enter End Date.");
 alert(dateDiffInDays(dateOne, dateTwo));
+
+
+
+
+	
